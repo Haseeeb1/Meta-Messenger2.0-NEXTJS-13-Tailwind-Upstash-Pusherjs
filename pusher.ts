e34ca665ca011ef96b533dfaedae169739173c4d
Haseeb Ul Hassan <100109683+Haseeeb1@users.noreply.github.com>
2022-11-11 21:21:24 +0500

    Update pusher.ts

--- pusher.ts
+++ pusher.ts
@@ -2,14 +2,11 @@ import Pusher from 'pusher';
 import ClientPusher from 'pusher-js';
 
 export const serverPusher=new Pusher({
-    appId: "1505511",
-  key: "7856d6be78eb5d4c8086",
-  secret: "96a41146d32d18ead30a",
-  cluster: "ap2",
+    //YOUR PUSHER CONFIG
   useTLS: true
 })
 
 export const clientPusher=new ClientPusher(
-    '7856d6be78eb5d4c8086', {
+    'Your pusher client key', {
         cluster: 'ap2'
 })
